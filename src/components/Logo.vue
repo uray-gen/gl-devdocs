21431c0062aebd79dcb2fb60f960e92d0e2a59dd
TechBread <66066723+ovendev@users.noreply.github.com>
2023-03-11 00:40:21 +0300

    Update Logo.vue

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,10 +1,10 @@
 <template>
-	<h4
+	<h1
 		width="100%"
 		height="100%"
 		class="filter drop-shadow-lg my-6"
-		style="color: white; font-size: 120px; font-weight: bold"
+		style="color: white; font-size: 80px; font-weight: bold"
 	>
 		Game Lobby Docs
-	</h4>
+	</h1>
 </template>
